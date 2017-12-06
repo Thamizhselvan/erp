aa0f7c6e56bfa20a198b5f5895811d9086e5b648
Thamizhselvan <tamil.oakdev@gmail.com>
2017-12-07 00:29:50 +0530

    updated

--- application/views/salarySlip.php
+++ application/views/salarySlip.php
@@ -1,68 +1,164 @@
+
+<form role="form">
+	 <div id="page-wrapper">
+            <div id="page-inner">
+				<div class="row">
+                    <div class="col-md-12">
+                        <h1 class="page-head-line">Salaryslip</h1>
+                       
+                    </div>
+                </div>
+                   <!-- /. ROW  -->
+                <div class="row">
+			                        
+                <div class="col-md-12 col-sm-12 col-xs-12">
+               <div class="panel panel-info">
+                        <div class="panel-heading">
+                          Salaryslip
+                        </div>
+                    <div class="panel-body">
+                               <div class="col-md-6"> 
+								   <div class="form-group">
+                                            <label >Department</label>
+                                             <div ><select id="dcode" class="form-control">
+														<option value="0">---Select---</option>
+														<?php foreach($deptList as $count): ?>
+														<option value="<?php echo $count->dcode; ?>"><?php echo $count->dname; ?></option>
+														<?php endforeach; ?> 
+													</select>
+											</div>
+                                           <!-- <input class="form-control" type="text"> -->
+                                          <!--  <p class="help-block">Help text here.</p> -->
+									</div>
+
+                               </div>
+                               <div class="col-md-6"> 
+								   <div class="form-group">
+                                            <label >Role</label>
+                                             <div ><select id="roleId" class="form-control">
+														<option value="0">---Select---</option>
+														<?php foreach($roleList as $count): ?>
+														<option value="<?php echo $count->role_id; ?>"><?php echo $count->role_name; ?></option>
+														<?php endforeach; ?> 
+													</select>
+											</div>  
+									</div>
+                               </div> 
+							   <div class="col-md-6"> 
+								   <div class="form-group">
+                                            <label >Employee</label>
+                                             <div ><select id="roleId" class="form-control">
+														<option value="0">---Select---</option>
+														<?php foreach($empList as $count): ?>
+														<option value="<?php echo $count->emp_id; ?>"><?php echo $count->emp_name; ?></option>
+														<?php endforeach; ?> 
+													</select>
+											</div>
+									</div>
+                               </div> 
+								<div class="col-md-6"> 
+								   <div class="form-group">
+                                            <label >Academic Year</label>
+                                             <div ><select id="academicYear" class="form-control" onchange="getAllAllowance()">
+														<option value="0">---Select---</option>
+														<option value="2016-2017">2016-2017</option>
+													</select>
+											</div>
+									</div>
+
+                               </div> 							   
+                                <div class="col-md-6">
+									 <div class="form-group">
+                                            <label >Month</label>
+                                            <div ><select id="month" class="form-control" >
+														<option value="0">---Select---</option>
+														<?php
+														$month = array( 1=>'JAN', 2=>'FEB', 3=>'MAR', 4=>'APR', 5=>'MAY',6=>'JUN',7=>'JUL',8=>'AUG',9=>'SEP',10=>'OCT',11=>'NOV',12=>'DEC');
+														while (list($key, $value) = each($month)) { 
+														?>
+															<option value="<?php echo $key?>"><?php echo $value?></option>
+														<?php } ?>
+													</select>
+													<select id="month" class="form-control" >
+														<option value="0">---Select---</option>
+														<?php
+															for( $year=2010;$year<=2017;$year++ ) {?>
+																<option value="<?php echo $year?>"><?php echo $year?></option>
+														<?php } ?>
+													</select>
+											</div>
+											
+                                     </div>
+                                </div>
+                                
+								
+								<div class="row">
+					 	 
+						<div class="col-md-12 table-responsive">
+						    
+                            <table  id="mytable"  class="table table-striped table-bordered table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Particulars</th>
+                            			<th>Amount</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+								</tbody>
+                            </table>
+                        </div>
+				<div class="col-md-12 text-center">
+						<ul class="pagination pagination-lg pager" id="myPager"></ul>
+				</div>
+		</div>
+                                
+                                
+                                <div class="col-md-12">
+                                <div class="col-md-3"></div>
+                                <div class="col-md-3">
+									 <div class="form-group">                                            
+                                            <div>
+												<input type="hidden" class="form-control" name="option" id="option" value="save">												
+												<input type="button" class="btn btn-info form-control" value="generate" id="generate" onclick="salaryslip()">
+											</div>
+                                     <!-- <p class="help-block">Help text here.</p> -->
+                                     </div>
+                                </div>
+                                <div class="col-md-3">
+									 <div class="form-group">                                            
+                                            <div>											
+												<input type="button" class="btn btn-info form-control" value="send" id="send" onclick="send(this)">
+											</div>
+                                     <!-- <p class="help-block">Help text here.</p> -->
+                                     </div>
+                                </div>
+                                <div class="col-md-3"></div>   
+                                </div> 
+                    </div>
+                
+                        </div>
+                      </div>
+                </div>   
+		      </div>
+        </div>
+     </div>
+</form> <!--     
 <!DOCTYPE html>
 <html lang = "en">
 <head>
 	<title>Employee Details</title>
 	<script type = 'text/javascript' src = "../../js/jquery-3.1.1.js"></script> 
-	<script type = 'text/javascript' src = "../../js/salaryslip.js"></script> 
+	<script type = 'text/javascript' src = "../../js/global.js"></script> 
 </head>
 <body>
 <form>
-	Department
-	  <select id="dcode">
-          <option value="newDept">---New---</option>
-		  <?php foreach($deptList as $count): ?>
-		     <option value="<?php echo $count->dcode; ?>"><?php echo $count->dname; ?></option>
-		  <?php endforeach; ?> 
-          </select><br>
-	Role:
-	    <select id="role">
-          <option value="0">---New---</option>
-		  <?php foreach($roleList as $count): ?>
-		     <option value="<?php echo $count->role_id; ?>"><?php echo $count->role_name; ?></option>
-		  <?php endforeach; ?> 
-        </select><br>
-	Employee:
-	    <select id="empId">
-          <option value="0">---New---</option>
-		  <?php foreach($empList as $count): ?>
-		     <option value="<?php echo $count->emp_id; ?>"><?php echo $count->emp_name; ?></option>
-		  <?php endforeach; ?> 
-        </select><br>
-	Pay slip for: 
-		Month
-			<select id="month">
-			  <option value="0">---New---</option>
-			  <?php
-				$month = array( 1=>'JAN', 2=>'FEB', 3=>'MAR', 4=>'APR', 5=>'MAY',6=>'JUN',7=>'JUL',8=>'AUG',9=>'SEP',10=>'OCT',11=>'NOV',12=>'DEC');
-				while (list($key, $value) = each($month)) { 
-				?>
-					<option value="<?php echo $key?>"><?php echo $value?></option>
-				<?php } ?>
-			</select>
-			
-		Year
-			<select id="year">
-			  <option value="0">---New---</option>
-			  <?php
-				for( $year=2010;$year<=2017;$year++ ) {?>
-					<option value="<?php echo $year?>"><?php echo $year?></option>
-			<?php } ?>
-			</select><br>
-	  Academic Year
-	  <input type="text" id="academicYear" value="2016-2017"><br>
-	  <table id="allowance">
-		<tr><th>Description</th><th>Amount</th></tr>
-	  </table>
-	  <input type="text" name="option" id="option" value="generate"><br>
-	  <input type="button" value="generate" id="generate" onclick="salaryslip()">
-	  <input type="button" value="send" id="send" onclick="send(this)">
+	-->
+	 
+	
+	 
 	  
-	  <div id="allowance1">
-			
-	  </div>
-	  <div id="deduction1">
-			
-	  </div>
-</form>
+<!--</form>
 </body>
 </html>
+-->
