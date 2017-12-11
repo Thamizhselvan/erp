ebbdbb2be0f0bb9f36c6acc5c6be8ad94d32d20e
Thamizhselvan <tamil.oakdev@gmail.com>
2017-12-11 23:06:19 +0530

    user setup

--- application/models/SettingsDAO.php
+++ application/models/SettingsDAO.php
@@ -9,5 +9,21 @@
 		  $this->db->insert('tbl_college', $data);
 		  return ($this->db->affected_rows() != 1) ? false : true;
 	  }
+	  public function saveProfile($data){
+		  $this->db->insert('tbl_userprofile', $data);
+		  return ($this->db->affected_rows() != 1) ? false : true;
+	  }
+	  public function updateProfile($data,$userId){
+		$this->db->where('username',$userId);
+	    $this->db->update('tbl_userprofile',$data);
+		return ($this->db->affected_rows() != 1) ? false : true;
+	  }
+	  public function profileCheck($username){
+		
+		$query = $this->db->query('SELECT count(user_id) as usercount FROM tbl_userprofile where username="'.$username.'"');
+		$row = $query->row();
+		return $row->usercount;
+	  }
+	  
    } 
 ?> 
